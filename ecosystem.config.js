94b7ae8d62bfdb64bf4d288a05b8ea199d0d2424
Steffen Reimann <steffenreimann2011@googlemail.com>
2022-02-24 13:19:06 +0100

    +

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	apps: [
 		{
 			name: 'NodeJS Webhook',
-			script: `./Watcher/webhook.js `,
+			script: `./Watcher/webhook.js ${DoInspect('WATCHER')}`,
 			watch: false,
 			env: {
 				NODE_ENV: 'development'
@@ -10,7 +10,7 @@ module.exports = {
 		},
 		{
 			name: 'NodeJS Server',
-			script: `./Server/app.js `,
+			script: `./Server/app.js ${DoInspect('SERVER')}`,
 			watch: false,
 			env: {
 				NODE_ENV: 'development'
@@ -18,3 +18,16 @@ module.exports = {
 		}
 	]
 };
+
+function DoInspect(type) {
+	console.log('DoInspect WATCHER_INSPECT', process.env.WATCHER_INSPECT);
+	console.log('DoInspect SERVER_INSPECT', process.env.SERVER_INSPECT);
+	let out = '';
+	if (type == 'SERVER') {
+		out = `--inspect=0.0.0.0 --inspect-port=${process.env.SERVER_INSPECT_PORT}`;
+	} else if (type == 'WATCHER') {
+		out = `--inspect=0.0.0.0 --inspect-port=${process.env.WATCHER_INSPECT_PORT}`;
+	}
+	console.log('DoInspect out', out);
+	return out;
+}
