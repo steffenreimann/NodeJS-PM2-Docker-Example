ca1c9c542058b7d2dbb2e031715418182f7185f9
Steffen Reimann <steffenreimann2011@googlemail.com>
2022-02-24 13:16:59 +0100

    +

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,16 +18,3 @@ module.exports = {
 		}
 	]
 };
-
-function DoInspect(type) {
-	console.log('DoInspect WATCHER_INSPECT', process.env.WATCHER_INSPECT);
-	console.log('DoInspect SERVER_INSPECT', process.env.SERVER_INSPECT);
-	let out = '';
-	if (type == 'SERVER') {
-		out = `--inspect=0.0.0.0 --inspect-port=${process.env.SERVER_INSPECT_PORT}`;
-	} else if (type == 'WATCHER') {
-		out = `--inspect=0.0.0.0 --inspect-port=${process.env.WATCHER_INSPECT_PORT}`;
-	}
-	console.log('DoInspect out', out);
-	return out;
-}
