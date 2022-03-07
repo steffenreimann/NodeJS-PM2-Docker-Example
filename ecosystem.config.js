7990aae8c7f0eaf509998d2f3e3be804a45d7561
Steffen Reimann <steffenreimann2011@googlemail.com>
2022-03-07 22:15:33 +0100

    +

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,42 +18,11 @@ module.exports = {
 		}
 	]
 };
-var fs = require('fs');
-var path = require('path');
-var walk = function(dir, done) {
-	var results = [];
-	fs.readdir(dir, function(err, list) {
-		if (err) return done(err);
-		var pending = list.length;
-		if (!pending) return done(null, results);
-		list.forEach(function(file) {
-			file = path.resolve(dir, file);
-			fs.stat(file, function(err, stat) {
-				if (stat && stat.isDirectory()) {
-					walk(file, function(err, res) {
-						results = results.concat(res);
-						if (!--pending) done(null, results);
-					});
-				} else {
-					results.push(file);
-					if (!--pending) done(null, results);
-				}
-			});
-		});
-	});
-};
 
 function DoInspect(type) {
 	//console.log('DoInspect WATCHER_INSPECT', process.env.WATCHER_INSPECT);
 	//console.log('DoInspect SERVER_INSPECT', process.env.SERVER_INSPECT);
-	//console.log(__dirname);
-	walk(__dirname, function(err, results) {
-		if (err) {
-			console.log('Error', err);
-		} else {
-			console.log('Results', results);
-		}
-	});
+	console.log(__dirname);
 
 	let out = '';
 	if (type == 'SERVER') {
