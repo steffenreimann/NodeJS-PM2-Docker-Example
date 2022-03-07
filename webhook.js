fa8d467bb01e492f6193be8c41ef71d8b8acba94
Steffen Reimann <steffenreimann2011@googlemail.com>
2022-03-07 21:02:43 +0100

    +

--- webhook.js
+++ webhook.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = process.env.GIT_WEBHOOK_PORT || 3001;
+//const port = process.env.GIT_WEBHOOK_PORT || 3001;
+const port = 3001;
 var pm2 = require('pm2');
 const simpleGit = require('simple-git');
 var path = require('path');
