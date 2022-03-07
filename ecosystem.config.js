bc8f337c772635be8d39b3a9855826f18c639d7a
Steffen Reimann <steffenreimann2011@googlemail.com>
2022-03-07 21:59:55 +0100

    +

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,29 +18,6 @@ module.exports = {
 		}
 	]
 };
-
-function DoInspect(type) {
-	//console.log('DoInspect WATCHER_INSPECT', process.env.WATCHER_INSPECT);
-	//console.log('DoInspect SERVER_INSPECT', process.env.SERVER_INSPECT);
-	//console.log(__dirname);
-	walk(__dirname, function(err, results) {
-		if (err) {
-			console.log('Error', err);
-		} else {
-			console.log('Results', results);
-		}
-	});
-
-	let out = '';
-	if (type == 'SERVER') {
-		out = `--inspect=0.0.0.0 --inspect-port=${process.env.SERVER_INSPECT_PORT}`;
-	} else if (type == 'WATCHER') {
-		out = `--inspect=0.0.0.0 --inspect-port=${process.env.WATCHER_INSPECT_PORT}`;
-	}
-	console.log('DoInspect out', out);
-	return out;
-}
-
 var fs = require('fs');
 var path = require('path');
 var walk = function(dir, done) {
@@ -65,3 +42,25 @@ var walk = function(dir, done) {
 		});
 	});
 };
+
+function DoInspect(type) {
+	//console.log('DoInspect WATCHER_INSPECT', process.env.WATCHER_INSPECT);
+	//console.log('DoInspect SERVER_INSPECT', process.env.SERVER_INSPECT);
+	//console.log(__dirname);
+	walk(__dirname, function(err, results) {
+		if (err) {
+			console.log('Error', err);
+		} else {
+			console.log('Results', results);
+		}
+	});
+
+	let out = '';
+	if (type == 'SERVER') {
+		out = `--inspect=0.0.0.0 --inspect-port=${process.env.SERVER_INSPECT_PORT}`;
+	} else if (type == 'WATCHER') {
+		out = `--inspect=0.0.0.0 --inspect-port=${process.env.WATCHER_INSPECT_PORT}`;
+	}
+	console.log('DoInspect out', out);
+	return out;
+}
