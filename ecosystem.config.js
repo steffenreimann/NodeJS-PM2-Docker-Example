3c77aab7e40ce410c181c52fedf1025385a09fde
Steffen Reimann <steffenreimann2011@googlemail.com>
2022-03-10 16:55:32 +0100

    +

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,16 +22,16 @@ module.exports = {
 function DoInspect(type) {
 	//console.log('DoInspect WATCHER_INSPECT', process.env.WATCHER_INSPECT);
 	//console.log('DoInspect SERVER_INSPECT', process.env.SERVER_INSPECT);
-	var ServerPort = process.env.SERVER_PORT || '3000';
-	var WatcherPort = process.env.WATCHER_PORT || '3001';
-	console.log('DoInspect ServerPort = ', ServerPort);
-	console.log('DoInspect WatcherPort = ', WatcherPort);
-	console.log(__dirname);
-
 	let out = '';
 	if (type == 'SERVER') {
+		let ServerPort = process.env.SERVER_PORT || '3000';
+		console.log('process.env.SERVER_PORT = ', process.env.SERVER_PORT);
+		console.log('DoInspect ServerPort = ', ServerPort);
 		out = `--inspect=0.0.0.0 --inspect-port=${ServerPort}`;
 	} else if (type == 'WATCHER') {
+		let WatcherPort = process.env.WATCHER_PORT || '3001';
+		console.log('process.env.WATCHER_PORT = ', process.env.WATCHER_PORT);
+		console.log('DoInspect WatcherPort = ', WatcherPort);
 		out = `--inspect=0.0.0.0 --inspect-port=${WatcherPort}`;
 	}
 	console.log('DoInspect out', out);
