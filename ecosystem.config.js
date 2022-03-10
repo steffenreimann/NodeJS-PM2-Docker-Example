4309cf1d03a66ab9c078e6aa9cdc96db4e7c6f9b
Steffen Reimann <steffenreimann2011@googlemail.com>
2022-03-10 16:45:12 +0100

    +

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	apps: [
 		{
 			name: 'NodeJS Webhook',
-			script: `./Watcher/webhook.js`,
+			script: `./Watcher/webhook.js ${DoInspect('WATCHER')}`,
 			watch: false,
 			env: {
 				NODE_ENV: 'development'
@@ -10,7 +10,7 @@ module.exports = {
 		},
 		{
 			name: 'NodeJS Server',
-			script: `./Server/app.js`,
+			script: `./Server/app.js ${DoInspect('SERVER')}`,
 			watch: false,
 			env: {
 				NODE_ENV: 'development'
@@ -22,13 +22,17 @@ module.exports = {
 function DoInspect(type) {
 	//console.log('DoInspect WATCHER_INSPECT', process.env.WATCHER_INSPECT);
 	//console.log('DoInspect SERVER_INSPECT', process.env.SERVER_INSPECT);
+	var ServerPort = process.env.SERVER_PORT || '3000';
+	var WatcherPort = process.env.WATCHER_PORT || '3001';
+	console.log('DoInspect ServerPort = ', ServerPort);
+	console.log('DoInspect WatcherPort = ', WatcherPort);
 	console.log(__dirname);
 
 	let out = '';
 	if (type == 'SERVER') {
-		out = `--inspect=0.0.0.0 --inspect-port=${process.env.SERVER_INSPECT_PORT}`;
+		out = `--inspect=0.0.0.0 --inspect-port=${ServerPort}`;
 	} else if (type == 'WATCHER') {
-		out = `--inspect=0.0.0.0 --inspect-port=${process.env.WATCHER_INSPECT_PORT}`;
+		out = `--inspect=0.0.0.0 --inspect-port=${WatcherPort}`;
 	}
 	console.log('DoInspect out', out);
 	return out;
